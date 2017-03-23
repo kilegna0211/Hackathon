dfccbbf2e1a4f236f181ce2cffda8b3e60bcfbcf
Angelique Clery <angeliqueclery@yahoo.fr>
2017-03-23 15:37:16 +0100

    add mean-stack

---
+++ generated/angular/template-cache.js
@@ -0,0 +1,161 @@
+angular.module("app").run(["$templateCache", function($templateCache) {
+
+  $templateCache.put("anon/home.html",
+    "<h1>Hello World</h1>\n" +
+    "<!DOCTYPE html>\n" +
+    "<html>\n" +
+    "\n" +
+    "<head>\n" +
+    "    <meta charset=\"utf-8\">\n" +
+    "    <link rel=\"stylesheet\" href=\"style.css\">\n" +
+    "\n" +
+    "    <title>Wild Quizz</title>\n" +
+    "</head>\n" +
+    "\n" +
+    "<body>\n" +
+    "    <header>\n" +
+    "\n" +
+    "        <img class=\"sun\" src=\"img/yellow_sun.svg\" alt=\"sun\">\n" +
+    "        <img class=\"cloud2\" src=\"img/cloud2.svg\" alt=\"cloud\" width=\"100px\" height=\"100px\">\n" +
+    "        <img class \"cloud1\" src=\"img/cloud1.svg\" alt=\"cloud\" width=\"110px\" height=\"110px\">\n" +
+    "\n" +
+    "\n" +
+    "    </header>\n" +
+    "\n" +
+    "    <section class=\"container\">\n" +
+    "        <p class=\"p1\"></p>\n" +
+    "        <p class=\"p2\"></p>\n" +
+    "        <p class=\"p3\"></p>\n" +
+    "\n" +
+    "    </section>\n" +
+    "\n" +
+    "\n" +
+    "    <footer>\n" +
+    "        <div id=\"squirrel\">\n" +
+    "            <div class=\"tail\"></div>\n" +
+    "            <div class=\"body\">\n" +
+    "                <div class=\"ears eleft\"></div>\n" +
+    "                <div class=\"ears eright\"></div>\n" +
+    "                <div class=\"eye left\"></div>\n" +
+    "                <div class=\"eye right\"></div>\n" +
+    "                <div class=\"mouth\"></div>\n" +
+    "                <div class=\"foot fleft\"></div>\n" +
+    "                <div class=\"foot fright\"></div>\n" +
+    "            </div>\n" +
+    "            <div class=\"food\"></div>\n" +
+    "        </div>\n" +
+    "    </footer>\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "</body>\n" +
+    "\n" +
+    "</html>\n"
+  );
+
+  $templateCache.put("anon/login.html",
+    "<div class=\"row\">\n" +
+    "    <div class=\"col-xs-6 col-xs-offset-3\">\n" +
+    "        <form class=\"form\" name=\"loginForm\" novalidate ng-submit=\"login()\">\n" +
+    "            <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>\n" +
+    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required placeholder=\"Email Address\">\n" +
+    "            </div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>\n" +
+    "                <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required placeholder=\"Password\">\n" +
+    "            </div>\n" +
+    "            <button type=\"submit\" class=\"btn btn-primary btn-block\">Login</button>\n" +
+    "        </form>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+  $templateCache.put("anon/navbar.html",
+    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
+    "    <div class=\"container-fluid\">\n" +
+    "        <div class=\"navbar-header\">\n" +
+    "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar\">\n" +
+    "        <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "      </button>\n" +
+    "            <a class=\"navbar-brand\" href=\"#\"></a>\n" +
+    "        </div>\n" +
+    "        <div class=\"collapse navbar-collapse\" id=\"navbar\">\n" +
+    "            <ul class=\"nav navbar-nav\">\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"anon.home\">Home</a></li>\n" +
+    "\n" +
+    "            </ul>\n" +
+    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "                <li>\n" +
+    "                    <li ui-sref-active=\"active\"><a ui-sref=\"anon.login\" ng-hide=\"auth.isAuthenticated()\">Login</a></li>\n" +
+    "                    <li ui-sref-active=\"active\"><a ui-sref=\"anon.register\" ng-hide=\"auth.isAuthenticated()\">Register</a></li>\n" +
+    "                    <li ui-sref-active=\"active\"><a ui-sref=\"user.dashboard\" ng-show=\"auth.isAuthenticated()\">Dashboard</a></li>\n" +
+    "                    <li><a ng-click=\"logout()\" ng-show=\"auth.isAuthenticated()\" href='#'>Logout</a></li>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</nav>\n"
+  );
+
+  $templateCache.put("anon/register.html",
+    "<div class=\"row\">\n" +
+    "    <div class=\"col-xs-6 col-xs-offset-3\">\n" +
+    "        <form class=\"form\" name=\"loginForm\" novalidate ng-submit=\"register()\">\n" +
+    "            <div ng-repeat=\"error in errors\">{{error.error}}</div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-user\"></i></span>\n" +
+    "                <input id=\"email\" type=\"email\" class=\"form-control\" ng-model=\"user.email\" required placeholder=\"Email Address\">\n" +
+    "            </div>\n" +
+    "            <div class=\"input-group\">\n" +
+    "                <span class=\"input-group-addon\"><i class=\"glyphicon glyphicon-lock\"></i></span>\n" +
+    "                <input id=\"password\" type=\"password\" class=\"form-control\" ng-model=\"user.password\" required placeholder=\"Password\">\n" +
+    "            </div>\n" +
+    "            <button type=\"submit\" class=\"btn btn-primary btn-block\">Register</button>\n" +
+    "        </form>\n" +
+    "    </div>\n" +
+    "</div>\n"
+  );
+
+  $templateCache.put("user/dashboard.html",
+    "Dashboard de {{user.email}}\n"
+  );
+
+  $templateCache.put("user/navbar.html",
+    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
+    "    <div class=\"container-fluid\">\n" +
+    "        <div class=\"navbar-header\">\n" +
+    "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar\">\n" +
+    "        <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "      </button>\n" +
+    "            <a class=\"navbar-brand\" href=\"#\"></a>\n" +
+    "        </div>\n" +
+    "        <div class=\"collapse navbar-collapse\" id=\"navbar\">\n" +
+    "            <ul class=\"nav navbar-nav\">\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.dashboard\" ng-show=\"auth.isAuthenticated()\">Dashboard</a></li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.profile\" ng-show=\"auth.isAuthenticated()\">Profile</a></li>\n" +
+    "\n" +
+    "            </ul>\n" +
+    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "                <li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"anon.home\">Website</a></li>\n" +
+    "                    <li><a ng-click=\"logout()\" ng-show=\"auth.isAuthenticated()\" href='#'>Logout</a></li>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</nav>\n"
+  );
+
+  $templateCache.put("user/profile.html",
+    "Profile de {{user.email}}\n"
+  );
+
+}]);
